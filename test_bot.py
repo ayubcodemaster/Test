d6edf7252b624a37b4060dc0adf1d150258e4c31
MukhammadAyub <ayubkhonibrokhimzoda@gmail.com>
2024-04-12 09:05:34 +0500

    deleted

--- test_bot.py
+++ test_bot.py
@@ -7,8 +7,7 @@ token = ''
 API = ""
 
 bot = telebot.TeleBot(token)
-def hello():
-    pass
+
 @bot.message_handler(commands=['start'])
 def start(message):
     markup = types.InlineKeyboardMarkup()
