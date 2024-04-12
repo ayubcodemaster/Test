6644e555add791a653d5e43f96576b4279fe19d6
ayubcodemaster <166776545+ayubcodemaster@users.noreply.github.com>
2024-04-12 07:03:08 +0500

    Update test_bot.py
    V2

--- test_bot.py
+++ test_bot.py
@@ -16,3 +16,25 @@ def start(message):
                types.InlineKeyboardButton('RUB', callback_data='RUB'))
 
     bot.send_message(message.chat.id, "Choose a currency:", reply_markup=markup)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def callback_query(call):
+    querystring = {"from": call.data, "to": "UZS", "amount": "1"}
+
+    headers = {
+        "X-RapidAPI-Key": "",
+        "X-RapidAPI-Host": "currency-converter-pro1.p.rapidapi.com"
+    }
+
+    response = requests.get(API, headers=headers, params=querystring)
+
+    if response.status_code == 200:
+        data = response.json()
+        print(data)
+        conversion_result = data['result']
+        bot.send_message(call.message.chat.id, f'Conversion result: {conversion_result}')
+    else:
+        bot.send_message(call.message.chat.id, 'Failed to perform conversion. Please try again later.')
+
+bot.polling()
